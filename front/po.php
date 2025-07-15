465bb4e14a5aa3ada3d0045e24be550d2c77513f
mackliu <macklun@gmail.com>
2025-07-15 13:34:34 +0800

    完成分類網誌功能

--- front/po.php
+++ front/po.php
@@ -9,20 +9,48 @@
     <div><a class='type-link' data-type='3'>癌症防治</a></div>
     <div><a class='type-link' data-type='4'>慢性病防治</a></div>
 </fieldset>
-<fieldset style="width:600px;display:inline-block">
+<fieldset style="width:590px;display:inline-block">
     <legend>文章列表</legend>
     <div id="TypeList"></div>
     <div id="Post"></div>
     
 </fieldset>
 <script>
+getList(1);
+
 $(".type-link").on("click",function(){
     let type=$(this).text();
     $("#NavType").text(type);
     let typeId=$(this).data("type");
-    $.get("api/get_type_list.php",{type:typeId},function(list){
+    getList(typeId);
+})
+
+function getPost(id){
+    $.get("./api/get_post.php",{id},function(post){
+            $("#TypeList").html("");
+            $("#Post").html(post)
+        })
+}
+
+
+$(".post-item").on("click",function(){
+    let postId=$(this).data("post");
+    getPost(postId);    
+})
+
+
+function getList(type){
+    $.get("api/get_type_list.php",{type},function(list){
         $("#Post").html("");
         $("#TypeList").html(list);
+        //console.log('list');
+        $(".post-item").on("click",function(){
+          //  console.log('post-item');
+                // 取得文章ID   
+          let postId=$(this).data("post");
+                
+                getPost(postId);    
+        })
     })
-})
+}
 </script>
